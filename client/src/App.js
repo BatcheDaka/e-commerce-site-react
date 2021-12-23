d15c7886211bf2d57a8939ab4baca8f47e4b434d
BatcheDaka <dakata@gmail.com>
2021-12-23 19:02:17 +0200

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import FooterPage from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <Header />
+      <FooterPage />
     </div>
   );
 }
---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+const FooterPage = () => {
+    return (
+      
+             <div>
+                <p>Pirotska Street</p>
+                <h3>Sofia, Bulgaria</h3>
+                <h4>+359 12345678</h4>
+                <p>pesho@abv.bg</p>
+             </div>
+          
+ 
+    );
+ }
+ 
+ 
+ export default FooterPage;
