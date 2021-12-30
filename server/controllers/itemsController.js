e291676436748640b5375a8cbf03230f4d973d02
BatcheDaka <dakata@gmail.com>
2021-12-30 12:26:55 +0200

    Update itemsController.js

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const Item = require('../models/item');
+const User = require('../models/user');
 const auth = require('../middlewears/auth')
 
 router.get('/items', (req, res) => {
     Item.find({}).then(items => {
         res.send(items);
+    })
+})
+router.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        res.send(users);
     })
 })
 
