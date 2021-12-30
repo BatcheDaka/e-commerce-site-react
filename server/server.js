72a8829aeb02845c94b8aa169f042b08933a6798
BatcheDaka <dakata@gmail.com>
2021-12-30 11:56:34 +0200

    updates

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const authController = require('./controllers/authController');
 const itemsController = require('./controllers/itemsController');
 
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -27,3 +26,4 @@ db();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
